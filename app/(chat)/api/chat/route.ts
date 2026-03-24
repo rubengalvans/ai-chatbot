84f917cda9cd442b96314aa3215f198140ffa993
rubengalvans <rubengalvans@gmail.com>
2026-03-23 22:23:55 -0600

    Update session_id to a hardcoded value

--- app/(chat)/api/chat/route.ts
+++ app/(chat)/api/chat/route.ts
@@ -26,7 +26,7 @@ export async function POST(req: Request) {
         },
         body: JSON.stringify({
           message: userMessage,
-          session_id: sessionId,
+          session_id: "1597868333",
         }),
       }
     );
